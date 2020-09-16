bfc77ccaef4777c8ab03fc12e494c6cafe3e7680
wanxwang <wanxin-wang@uiowa.edu>
2020-09-16 18:39:12 -0500

    add aditional joint

--- d2_path_tracing_script.m
+++ d2_path_tracing_script.m
@@ -17,9 +17,16 @@ joint_2.JointAxis = [0 0 1];
 body_2.Joint = joint_2;
 addBody(robot, body_2, 'link1');
 
+%body_2_2 = rigidBody('link2_2');
+%joint_2_2 = rigidBodyJoint('joint2_2','revolute');
+%setFixedTransform(joint_2_2, trvec2tform([0, 0, 0]));
+%joint_2_2.JointAxis = [0 1 0];
+%body_2_2.Joint = joint_2_2;
+%addBody(robot, body_2_2, 'link2');
+
 body_3 = rigidBody('tool');
 joint_3 = rigidBodyJoint('fix1','fixed');
-setFixedTransform(joint_3, trvec2tform([0, L2, 0]));
+setFixedTransform(joint_3, trvec2tform([0, -L2, 0]));
 body_3.Joint = joint_3;
 addBody(robot, body_3, 'link2');
 
@@ -38,8 +45,9 @@ q0 = homeConfiguration(robot);      %?
 ndof = length(q0);
 qs = zeros(count, ndof);
 
+
 start_0 = tic;
-ik = inverseKinematics('RigidBodyTree', robot, 'SolverAlgorithm', 'BFGSGradientProjection');
+ik = inverseKinematics('RigidBodyTree', robot, 'SolverAlgorithm', 'LevenbergMarquardt');
 weights = [0, 0, 0, 1, 1, 0];       %?
 endEffector = 'tool';
 endEffector_confs = zeros(4, 4, count);
@@ -54,7 +62,7 @@ for i = 1:count
     endEffector_confs(:, :, i) = trvec2tform(point);
     %assert(isequal(endEffector_confs(1:3, 1:3, i), eye(3)));
 
-    qSol = ik(endEffector, endEffector_confs(:, :, i), weights, qInitial);
+    [qSol, solInfo(i)] = ik(endEffector, endEffector_confs(:, :, i), weights, qInitial);
     % Store the configuration
     qs(i,:) = qSol;
     % Start from prior solution
@@ -63,6 +71,7 @@ end
 elapsedTime_1 = toc(start_1);
 figure
 show(robot,qs(1,:)');				%?
+%show(robot,q0);               %?
 view(2)								%?
 ax = gca;							%?
 ax.Projection = 'orthographic';		%?
@@ -70,12 +79,13 @@ hold on
 plot(points(:,1),points(:,2),'k')
 axis([-0.1 0.7 -0.3 0.5])
 
-% framesPerSecond = 15;
-% r = rateControl(framesPerSecond);
-% for i = 1:count
-%     show(robot,qs(i,:)','PreservePlot',false);
-%     drawnow
-%     waitfor(r);
-% end
+framesPerSecond = 15;
+r = rateControl(framesPerSecond);
+for i = 1:count
+    show(robot,qs(i,:)','PreservePlot',false);
+    drawnow
+    waitfor(r);
+end
 
 writematrix(qs, 'ik_solutions.csv');
+writetable(struct2table(solInfo), 'ik_solutions_info.txt');
