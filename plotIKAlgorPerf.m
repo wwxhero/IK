8f10e7734e06e01de069ed09ea7cc060c5ff86cb
wanxwang <wanxin-wang@uiowa.edu>
2020-12-26 21:34:44 -0600

    plot is done for comparing constraint and unconstraint

--- plotIKAlgorPerf.m
+++ plotIKAlgorPerf.m
@@ -1,5 +1,6 @@
 function plotIKAlgorPerf(Theta_2, Err_1, strLengend_1 ...
-						, Theta_2_prime, Err_1_prime, strLengend_2)
+						, Theta_2_prime, Err_1_prime, strLengend_2 ...
+						, c_theta_2)
 	[n_iter, ~] = size(Theta_2);
 	[n_iter_prime, ~] = size(Theta_2_prime);
 	abs_delta = zeros(1, n_iter);
@@ -18,12 +19,24 @@ function plotIKAlgorPerf(Theta_2, Err_1, strLengend_1 ...
 	end
 	abs_delta_prime(1) = nan;
 
+	min_theta_1 = min(Theta_2(:, 1));
+	max_theta_1 = max(Theta_2(:, 1));
+	step_theta_1 = (max_theta_1 - min_theta_1)/min(n_iter, n_iter_prime);
+	Theta_1 = min_theta_1:step_theta_1:max_theta_1;
+	Theta_2_limit = Theta_1;
+	if nargin < 7
+		Theta_2_limit(1:end) = nan;
+	else
+		Theta_2_limit(1:end) = c_theta_2;
+	end
+
 	figure
 	n_row_plot = 3;
 	subplot(n_row_plot, 1, 1);
 
 	plot(Theta_2(1:n_iter, 1)', Theta_2(1:n_iter, 2)', 'r*' ...
-		, Theta_2_prime(1:n_iter_prime, 1)', Theta_2_prime(1:n_iter_prime, 2)', 'bo');
+		, Theta_2_prime(1:n_iter_prime, 1)', Theta_2_prime(1:n_iter_prime, 2)', 'bo' ...
+		, Theta_1, Theta_2_limit, 'g-');
 	title('Iterative Solutions');
 	xlabel('\theta_1');
 	ylabel('\theta_2');
