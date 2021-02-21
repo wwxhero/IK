72e6b0a2dd82b76eb9e8a5243eea02cb373d6339
wanxwang <wanxin-wang@uiowa.edu>
2021-02-21 12:57:36 -0600

    Inspecting Joint through FK spec

--- GeneralizedInverseKinematicsExample.m
+++ GeneralizedInverseKinematicsExample.m
@@ -1,125 +1,137 @@
 %% Plan a Reaching Trajectory With Multiple Kinematic Constraints
-% This example shows how to use generalized inverse kinematics to plan a joint-space 
-% trajectory for a robotic manipulator. It combines multiple constraints to generate 
-% a trajectory that guides the gripper to a cup resting on a table. These constraints 
-% ensure that the gripper approaches the cup in a straight line and that the gripper 
-% remains at a safe distance from the table, without requiring the poses of the 
+% This example shows how to use generalized inverse kinematics to plan a joint-space
+% trajectory for a robotic manipulator. It combines multiple constraints to generate
+% a trajectory that guides the gripper to a cup resting on a table. These constraints
+% ensure that the gripper approaches the cup in a straight line and that the gripper
+% remains at a safe distance from the table, without requiring the poses of the
 % gripper to be determined in advance.
 %% Set Up the Robot Model
-% This example uses a model of the KUKA LBR iiwa, a 7 degree-of-freedom robot 
-% manipulator. <docid:robotics_ref.bvlvwcs-1 |importrobot|> generates a <docid:robotics_ref.bvan8uq-1 
-% |rigidBodyTree|> model from a description stored in a Unified Robot Description 
+% This example uses a model of the KUKA LBR iiwa, a 7 degree-of-freedom robot
+% manipulator. <docid:robotics_ref.bvlvwcs-1 |importrobot|> generates a <docid:robotics_ref.bvan8uq-1
+% |rigidBodyTree|> model from a description stored in a Unified Robot Description
 % Format (URDF) file.
 
 lbr = importrobot('iiwa14.urdf'); % 14 kg payload version
 lbr.DataFormat = 'row';
+
+bodies = lbr.Bodies;
+[~, n_bodies] = size(bodies);
+
+for i_bodies = 1 : n_bodies
+  joint_i = bodies{i_bodies}.Joint;
+  fprintf('%s: [%4.2f %4.2f %4.2f]\n' ...
+        , joint_i.Name ...
+        , joint_i.JointAxis(1), joint_i.JointAxis(2), joint_i.JointAxis(3));
+end
+
+
 gripper = 'iiwa_link_ee_kuka';
-%% 
+%%
 % Define dimensions for the cup.
 
 cupHeight = 0.2;
 cupRadius = 0.05;
 cupPosition = [-0.5, 0.5, cupHeight/2];
-%% 
+%%
 % Add a fixed body to the robot model representing the center of the cup.
 
 body = rigidBody('cupFrame');
 setFixedTransform(body.Joint, trvec2tform(cupPosition))
 addBody(lbr, body, lbr.BaseName);
 %% Define the Planning Problem
-% The goal of this example is to generate a sequence of robot configurations 
+% The goal of this example is to generate a sequence of robot configurations
 % that satisfy the following criteria:
-%% 
+%%
 % * Start in the home configuration
 % * No abrupt changes in robot configuration
 % * Keep the gripper at least 5 cm above the "table" (z = 0)
 % * The gripper should be aligned with the cup as it approaches
 % * Finish with the gripper 5 cm from the center of the cup
-%% 
-% This example utilizes constraint objects to generate robot configurations 
-% that satisfy these criteria. The generated trajectory consists of five configuration 
-% waypoints. The first waypoint, |q0|, is set as the home configuration. Pre-allocate 
+%%
+% This example utilizes constraint objects to generate robot configurations
+% that satisfy these criteria. The generated trajectory consists of five configuration
+% waypoints. The first waypoint, |q0|, is set as the home configuration. Pre-allocate
 % the rest of the configurations in |qWaypoints| using |repmat|.
 
 numWaypoints = 5;
 q0 = homeConfiguration(lbr);
 qWaypoints = repmat(q0, numWaypoints, 1);
-%% 
-% Create a <docid:robotics_ref.bvlvs3d-1 |generalizedInverseKinematics|> solver 
+%%
+% Create a <docid:robotics_ref.bvlvs3d-1 |generalizedInverseKinematics|> solver
 % that accepts the following constraint inputs:
-%% 
+%%
 % * Cartesian bounds - Limits the height of the gripper
 % * A position target - Specifies the position of the cup relative to the gripper.
 % * An aiming constraint - Aligns the gripper with the cup axis
-% * An orientation target - Maintains a fixed orientation for the gripper while 
+% * An orientation target - Maintains a fixed orientation for the gripper while
 % approaching the cup
 % * Joint position bounds - Limits the change in joint positions between waypoints.
 
 gik = generalizedInverseKinematics('RigidBodyTree', lbr, ...
     'ConstraintInputs', {'cartesian','position','aiming','orientation','joint'})
 %% Create Constraint Objects
-% Create the constraint objects that are passed as inputs to the solver. These 
-% object contain the parameters needed for each constraint. Modify these parameters 
+% Create the constraint objects that are passed as inputs to the solver. These
+% object contain the parameters needed for each constraint. Modify these parameters
 % between calls to the solver as necessary.
-% 
-% Create a Cartesian bounds constraint that requires the gripper to be at least 
-% 5 cm above the table (negative z direction). All other values are given as |inf| 
+%
+% Create a Cartesian bounds constraint that requires the gripper to be at least
+% 5 cm above the table (negative z direction). All other values are given as |inf|
 % or |-inf|.
 
 heightAboveTable = constraintCartesianBounds(gripper);
 heightAboveTable.Bounds = [-inf, inf; ...
                            -inf, inf; ...
                            0.05, inf]
-%% 
-% Create a constraint on the position of the cup relative to the gripper, with 
+%%
+% Create a constraint on the position of the cup relative to the gripper, with
 % a tolerance of 5 mm.
 
 distanceFromCup = constraintPositionTarget('cupFrame');
 distanceFromCup.ReferenceBody = gripper;
 distanceFromCup.PositionTolerance = 0.005
-%% 
-% Create an aiming constraint that requires the z-axis of the |iiwa_link_ee| 
-% frame to be approximately vertical, by placing the target far above the robot. 
-% The |iiwa_link_ee| frame is oriented such that this constraint aligns the gripper 
+%%
+% Create an aiming constraint that requires the z-axis of the |iiwa_link_ee|
+% frame to be approximately vertical, by placing the target far above the robot.
+% The |iiwa_link_ee| frame is oriented such that this constraint aligns the gripper
 % with the axis of the cup.
 
 alignWithCup = constraintAiming('iiwa_link_ee');
 alignWithCup.TargetPoint = [0, 0, 100]
-%% 
-% Create a joint position bounds constraint. Set the |Bounds| property of this 
-% constraint based on the previous configuration to limit the change in joint 
+%%
+% Create a joint position bounds constraint. Set the |Bounds| property of this
+% constraint based on the previous configuration to limit the change in joint
 % positions.
 
 limitJointChange = constraintJointBounds(lbr)
-%% 
-% Create an orientation constraint for the gripper with a tolerance of one degree. 
-% This constraint requires the orientation of the gripper to match the value specified 
-% by the |TargetOrientation| property. Use this constraint to fix the orientation 
+%%
+% Create an orientation constraint for the gripper with a tolerance of one degree.
+% This constraint requires the orientation of the gripper to match the value specified
+% by the |TargetOrientation| property. Use this constraint to fix the orientation
 % of the gripper during the final approach to the cup.
 
 fixOrientation = constraintOrientationTarget(gripper);
 fixOrientation.OrientationTolerance = deg2rad(1)
 %% Find a Configuration That Points at the Cup
-% This configuration should place the gripper at a distance from the cup, so 
+% This configuration should place the gripper at a distance from the cup, so
 % that the final approach can be made with the gripper properly aligned.
 
 intermediateDistance = 0.3;
-%% 
-% Constraint objects have a |Weights| property which determines how the solver 
-% treats conflicting constraints. Setting the weights of a constraint to zero 
-% disables the constraint. For this configuration, disable the joint position 
+%%
+% Constraint objects have a |Weights| property which determines how the solver
+% treats conflicting constraints. Setting the weights of a constraint to zero
+% disables the constraint. For this configuration, disable the joint position
 % bounds and orientation constraint.
 
 limitJointChange.Weights = zeros(size(limitJointChange.Weights));
 fixOrientation.Weights = 0;
-%% 
-% Set the target position for the cup in the gripper frame. The cup should lie 
+%%
+% Set the target position for the cup in the gripper frame. The cup should lie
 % on the z-axis of the gripper at the specified distance.
 
 distanceFromCup.TargetPosition = [0,0,intermediateDistance];
-%% 
-% Solve for the robot configuration that satisfies the input constraints using 
-% the |gik| solver. You must specify all the input constraints. Set that configuration 
+%%
+% Solve for the robot configuration that satisfies the input constraints using
+% the |gik| solver. You must specify all the input constraints. Set that configuration
 % as the second waypoint.
 
 [qWaypoints(2,:),solutionInfo] = gik(q0, heightAboveTable, ...
@@ -130,28 +142,28 @@ distanceFromCup.TargetPosition = [0,0,intermediateDistance];
 
 limitJointChange.Weights = ones(size(limitJointChange.Weights));
 fixOrientation.Weights = 1;
-%% 
-% Disable the align-with-cup constraint, as the orientation constraint makes 
+%%
+% Disable the align-with-cup constraint, as the orientation constraint makes
 % it redundant.
 
 alignWithCup.Weights = 0;
-%% 
-% Set the orientation constraint to hold the orientation based on the previous 
-% configuration (|qWaypoints(2,:)|). Get the transformation from the gripper to 
+%%
+% Set the orientation constraint to hold the orientation based on the previous
+% configuration (|qWaypoints(2,:)|). Get the transformation from the gripper to
 % the base of the robot model. Convert the homogeneous transformation to a quaternion.
 
 fixOrientation.TargetOrientation = ...
     tform2quat(getTransform(lbr,qWaypoints(2,:),gripper));
-%% 
+%%
 % Define the distance between the cup and gripper for each waypoint
 
 finalDistanceFromCup = 0.05;
 distanceFromCupValues = linspace(intermediateDistance, finalDistanceFromCup, numWaypoints-1);
-%% 
+%%
 % Define the maximum allowed change in joint positions between each waypoint.
 
 maxJointChange = deg2rad(10);
-%% 
+%%
 % Call the solver for each remaining waypoint.
 
 for k = 3:numWaypoints
@@ -167,7 +179,7 @@ for k = 3:numWaypoints
                                          fixOrientation, limitJointChange);
 end
 %% Visualize the Generated Trajectory
-% Interpolate between the waypoints to generate a smooth trajectory. Use <docid:matlab_ref.bvjbxbd 
+% Interpolate between the waypoints to generate a smooth trajectory. Use <docid:matlab_ref.bvjbxbd
 % |pchip|> to avoid overshoots, which might violate the joint limits of the robot.
 
 framerate = 15;
@@ -176,7 +188,7 @@ tFinal = 10;
 tWaypoints = [0,linspace(tFinal/2,tFinal,size(qWaypoints,1)-1)];
 numFrames = tFinal*framerate;
 qInterp = pchip(tWaypoints,qWaypoints',linspace(0,tFinal,numFrames))';
-%% 
+%%
 % Compute the gripper position for each interpolated configuration.
 
 gripperPosition = zeros(numFrames,3);
@@ -184,7 +196,7 @@ for k = 1:numFrames
     gripperPosition(k,:) = tform2trvec(getTransform(lbr,qInterp(k,:), ...
                                                     gripper));
 end
-%% 
+%%
 % Show the robot in its initial configuration along with the table and cup
 
 figure;
@@ -192,7 +204,7 @@ show(lbr, qWaypoints(1,:), 'PreservePlot', false);
 hold on
 exampleHelperPlotCupAndTable(cupHeight, cupRadius, cupPosition);
 p = plot3(gripperPosition(1,1), gripperPosition(1,2), gripperPosition(1,3));
-%% 
+%%
 % Animate the manipulator and plot the gripper position.
 
 hold on
@@ -204,10 +216,10 @@ for k = 1:size(qInterp,1)
     waitfor(r);
 end
 hold off
-%% 
-% If you want to save the generated configurations to a MAT-file for later use, 
+%%
+% If you want to save the generated configurations to a MAT-file for later use,
 % execute the following:
-% 
+%
 % |>> save('lbr_trajectory.mat', 'tWaypoints', 'qWaypoints');|
-% 
+%
 % _Copyright 2016 The MathWorks, Inc._
--- SolveInverseKinematicsForAFourBarLinkageExample.m
+++ SolveInverseKinematicsForAFourBarLinkageExample.m
@@ -1,19 +1,19 @@
 %% Solve Inverse Kinematics for a Four-Bar Linkage
-% This example shows how to solve inverse kinematics for a four-bar linkage, 
-% a simple planar closed-chain linkage. Robotics System Toolbox™ doesn't directly 
-% support closed-loop mechanisms. However, the loop-closing joints can be approximated 
-% using kinematic constraints. This example shows how to setup a rigid body tree 
-% for a four-bar linkage, specify the kinematic constraints, and solve for a desired 
+% This example shows how to solve inverse kinematics for a four-bar linkage,
+% a simple planar closed-chain linkage. Robotics System Toolbox™ doesn't directly
+% support closed-loop mechanisms. However, the loop-closing joints can be approximated
+% using kinematic constraints. This example shows how to setup a rigid body tree
+% for a four-bar linkage, specify the kinematic constraints, and solve for a desired
 % end-effector position.
-% 
-% Initialize the four-bar linkage rigid body tree model. 
+%
+% Initialize the four-bar linkage rigid body tree model.
 
 robot = rigidBodyTree('Dataformat','column','MaxNumBodies',7);
-%% 
-% Define body names, parent names, joint names, joint types, and fixed transforms 
-% in cell arrays. The fixed transforms define the geometry of the four-bar linkage. 
-% The linkage rotates in the _xz_-plane. An offset of |-0.1| is used in the _y_-axis 
-% on the  |'b4'| body to isolate the motion of the overlapping joints for |'b3'| 
+%%
+% Define body names, parent names, joint names, joint types, and fixed transforms
+% in cell arrays. The fixed transforms define the geometry of the four-bar linkage.
+% The linkage rotates in the _xz_-plane. An offset of |-0.1| is used in the _y_-axis
+% on the  |'b4'| body to isolate the motion of the overlapping joints for |'b3'|
 % and |'b4'|.
 
 bodyNames = {'b1','b2','b3','b4','b5','b6'};
@@ -26,9 +26,9 @@ fixedTforms = {eye(4), ...
                 trvec2tform([0.0 -0.1 0]), ...
                 trvec2tform([0.8 0 0]), ...
                 trvec2tform([0 0 0.5])};
-%% 
+%%
 % Use a |for| loop to assemble the four-bar linkage:
-%% 
+%%
 % * Create a rigid body and specify the joint type.
 % * Specify the |JointAxis| property for any non-fixed joints.
 % * Specify the fixed transformation.
@@ -38,35 +38,35 @@ for k = 1:6
 
     b = rigidBody(bodyNames{k});
     b.Joint = rigidBodyJoint(jointNames{k},jointTypes{k});
-    
+
     if ~strcmp(jointTypes{k},'fixed')
         b.Joint.JointAxis = [0 1 0];
     end
-    
+
     b.Joint.setFixedTransform(fixedTforms{k});
-    
+
     addBody(robot,b,parentNames{k});
 end
-%% 
-% Add a final body to function as the end-effector (handle) for the four-bar 
+%%
+% Add a final body to function as the end-effector (handle) for the four-bar
 % linkage.
 
 bn = 'handle';
 b = rigidBody(bn);
 setFixedTransform(b.Joint,trvec2tform([0 -0.15 0]));
 addBody(robot,b,'b6');
-%% 
+%%
 % Specify kinematic constraints for the |GeneralizedInverseKinematics| object:
-%% 
-% * *Position constraint 1* : The origins of |'b3'| body frame and |'b6'| body 
-% frame should always overlap. This keeps the handle in line with the approximated 
+%%
+% * *Position constraint 1* : The origins of |'b3'| body frame and |'b6'| body
+% frame should always overlap. This keeps the handle in line with the approximated
 % closed-loop mechanism. Use the |-0.1| offset for the _y_-coordinate.
 % * *Position constraint 2* : End-effector should target the desired position.
 % * *Joint limit bounds* : Satisfy the joint limits in the rigid body tree model.
 
 gik = generalizedInverseKinematics('RigidBodyTree',robot);
 gik.ConstraintInputs = {'position',...  % Position constraint for closed-loop mechanism
-                        'position',...  % Position constraint for end-effector 
+                        'position',...  % Position constraint for end-effector
                         'joint'};       % Joint limits
 gik.SolverParameters.AllowRandomRestart = false;
 
@@ -83,17 +83,17 @@ jointLimBounds.Weights = ones(1,size(gik.RigidBodyTree.homeConfiguration,1))*10;
 % Position constraint 2
 desiredEEPosition = [0.9 -0.1 0.9]'; % Position is relative to base.
 positionTarget2 = constraintPositionTarget('handle');
-positionTarget2.TargetPosition = desiredEEPosition; 
+positionTarget2.TargetPosition = desiredEEPosition;
 positionTarget2.PositionTolerance = 1e-6;
 positionTarget2.Weights = 1;
-%% 
-% Compute the kinematic solution using the |gik| object. Specify the initial 
+%%
+% Compute the kinematic solution using the |gik| object. Specify the initial
 % guess and the different kinematic constraints in the proper order.
 
 iniGuess = homeConfiguration(robot);
 [q, solutionInfo] = gik(iniGuess,positionTarget1,positionTarget2,jointLimBounds);
-%% 
-% Examine the results in |solutionInfo|. Show the kinematic solution compared 
+%%
+% Examine the results in |solutionInfo|. Show the kinematic solution compared
 % to the home configuration. Plots are shown in the _xz_-plane.
 
 loopClosingViolation = solutionInfo.ConstraintViolations(1).Violation;
@@ -108,5 +108,18 @@ subplot(1,2,2)
 show(robot,q);
 title('GIK Solution')
 view([0 -1 0]);
-%% 
+
+
+[n_q, ~] = size(q);
+figure
+title('Inspeciting joint through FK spec');
+for i = 1 : n_q
+    subplot(1, n_q, i)
+    q_prime = q;
+    q_prime(i) = 0;
+    show(robot, q_prime);
+    title(sprintf('\\Delta\\theta_%d=0', i));
+    view([0, -1, 0]);
+end
+%%
 % _Copyright 2019 The MathWorks, Inc._
